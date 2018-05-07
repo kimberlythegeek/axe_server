12ea3196146d2595b932db0a8b96f7210d91b00d
Kimberly <ksereduck@mozilla.com>
2018-05-07 13:35:31 -0600

    fetch only certain columns

--- axe_server/api/api.py
+++ axe_server/api/api.py
@@ -23,7 +23,7 @@ class Results(Resource):
         with sql.connect(DATABASE) as db:
             c = db.cursor()
             query = """
-                SELECT * FROM data WHERE name = \"{}\" ORDER BY updated
+                SELECT updated, json FROM data WHERE name = \"{}\" ORDER BY updated
                 """.format(site_name)
             c.execute(query)
             rows = c.fetchall()
